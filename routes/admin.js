7f9611e50e8b74472b17d211a9ecc757d5357fdb
Chandrachud <66238180+chandrasahi10@users.noreply.github.com>
2023-07-21 12:15:35 +0530

    Added path to html page

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 
 router.get('/add-product',(req,res,next)=>{
-    res.send('<form action="/admin/add-product" method="POST"><label>Title</label><br><input type="text" name="title"></input><br><label>Size</label><br><input type="number" name="size"></input><button type="submit">Add Product</button></form>');
+    res.sendFile(path.join(__dirname,'../','views','add-product.html'));
  });
  
  router.post('/add-product',(req,res,next)=>{
