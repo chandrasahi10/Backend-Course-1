11b473327515628767ade5a301e4c4acce18623b
Chandrachud <66238180+chandrasahi10@users.noreply.github.com>
2023-07-21 15:30:16 +0530

    Used static call for files

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+
 const app = express();
 
 const adminRoutes = require('./routes/admin');
@@ -11,6 +12,7 @@ const shopRoutes = require('./routes/shop');
 
 
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname,'public')));
 
 app.use(adminRoutes);
 app.use(shopRoutes);
