5601368deb02e502dcaca77284fe0496e621bbd8
Chandrachud <66238180+chandrasahi10@users.noreply.github.com>
2023-07-21 15:28:14 +0530

    Used helper function for path

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
+const rootDir = require('../util/path');
 const router = express.Router();
 
 router.get('/add-product',(req,res,next)=>{
-    res.sendFile(path.join(__dirname,'../','views','add-product.html'));
+    res.sendFile(path.join(rootDir,'views','add-product.html'));
  });
  
  router.post('/add-product',(req,res,next)=>{
- 
-     console.log(req.body);
-     res.redirect('/');
+      
+    fs.writeFile("product.txt",`${req.body.title}` ,(err)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.redirect('/');
+        }
+    })   
  });
 
 module.exports = router;
