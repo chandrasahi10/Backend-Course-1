742c091c66ba0566b0a6b65b21c5d2c4ad5334dc
Chandrachud <66238180+chandrasahi10@users.noreply.github.com>
2023-07-21 12:17:28 +0530

    Added path to html page

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
@@ -11,11 +12,11 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/admin',adminRoutes);
+app.use(adminRoutes);
 app.use(shopRoutes);
 
 app.use('/',(req,res,next)=>{
-    res.status(404).send('<h1>Error 404: Not Found</h1>');
+    res.status(404).sendFile(path.join(__dirname,'views','page-not-found.html'));
 });
 
 app.listen(3000);
