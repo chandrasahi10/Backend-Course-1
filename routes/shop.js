608a5c3abe1faf84a12960a066b8f689d300996f
Chandrachud <66238180+chandrasahi10@users.noreply.github.com>
2023-07-21 21:37:34 +0530

    Used controller

--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const path = require('path');
 
 const router = express.Router();
-const rootDir = require('../util/path');
 
-router.get('/',(req,res,next)=>{
-    res.sendFile(path.join(rootDir,'views','shop.html'));
-});
+const productController = require('../controllers/products');
+
+router.get('/',productController.getProduct);
 
 module.exports = router;
