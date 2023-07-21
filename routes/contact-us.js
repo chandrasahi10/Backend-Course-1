20038c7ebea92a2ed684dd0caa0c4eaaf032a5fb
Chandrachud <66238180+chandrasahi10@users.noreply.github.com>
2023-07-21 21:37:11 +0530

    Used controller

--- routes/contact-us.js
+++ routes/contact-us.js
@@ -1,23 +1,11 @@
 const express = require('express');
-const path = require('path');
-const fs = require('fs');
 
-const rootDir = require('../util/path');
 const router = express.Router();
 
-router.get('/contact-us',(req,res,next)=>{
-    res.sendFile(path.join(rootDir,'views','contact-us.html'));
- });
+const contactController = require('../controllers/contacts')
+
+router.get('/contact-us',contactController.getContact);
  
- router.post('/contact-us',(req,res,next)=>{
-      
-    fs.writeFile("User-contact.txt",`${req.body.name}:${req.body.email}` ,(err)=>{
-        if(err){
-            console.log(err);
-        }else{
-            res.redirect('/success');
-        }
-    })   
- });
+ router.post('/contact-us',contactController.postContact);
 
 module.exports = router;
