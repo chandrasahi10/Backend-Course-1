f674a79f1736c6231d4daafd57d60419973f71dd
Chandrachud <66238180+chandrasahi10@users.noreply.github.com>
2023-07-21 16:08:26 +0530

    Added contact page

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const app = express();
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-
+const contactRoutes = require('./routes/contact-us');
+const successRoutes = require('./routes/success');
 
 
 
@@ -16,6 +17,8 @@ app.use(express.static(path.join(__dirname,'public')));
 
 app.use(adminRoutes);
 app.use(shopRoutes);
+app.use(contactRoutes);
+app.use(successRoutes);
 
 app.use('/',(req,res,next)=>{
     res.status(404).sendFile(path.join(__dirname,'views','page-not-found.html'));
