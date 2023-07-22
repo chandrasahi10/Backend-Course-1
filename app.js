281dd9d2177782458b2aa9d279422ed595341890
Chandrachud <66238180+chandrasahi10@users.noreply.github.com>
2023-07-22 18:50:34 +0530

    Edited app.js

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
+
+const errorController = require('./controllers/error');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const contactRoutes = require('./routes/contact-us');
-const successRoutes = require('./routes/success');
-const errorController = require('./controllers/Error404');
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(express.static(path.join(__dirname,'public')));
-
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-app.use(contactRoutes);
-app.use(successRoutes);
 
-app.use('/',errorController.Error);
+app.use(errorController.get404);
 
 app.listen(3000);
