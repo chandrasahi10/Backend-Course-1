4e853aee1934bcdf2f28437a62feae491261133d
Chandrachud <66238180+chandrasahi10@users.noreply.github.com>
2023-07-22 18:36:12 +0530

    Used render

--- controllers/products.js
+++ controllers/products.js
@@ -1,22 +1,27 @@
-const path = require('path');
-const rootDir = require('../util/path');
-const fs = require('fs');
+const products = [];
 
-exports.getAddProduct= (req,res,next)=>{
-    res.sendFile(path.join(rootDir,'views','add-product.html'));
- };
+exports.getAddProduct = (req, res, next) => {
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
+};
 
-exports.postAddProduct = (req,res,next)=>{
-      
-    fs.writeFile("product.txt",`${req.body.title}` ,(err)=>{
-        if(err){
-            console.log(err);
-        }else{
-            res.redirect('/');
-        }
-    })   
- };
+exports.postAddProduct = (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect('/');
+};
 
-exports.getProduct = (req,res,next)=>{
-    res.sendFile(path.join(rootDir,'views','shop.html'));
+exports.getProducts = (req, res, next) => {
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
 };
